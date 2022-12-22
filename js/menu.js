83237773f3cfb30a9a6925930ad67457db8de6e4
Justin Duong <jduong@umich.edu>
2022-12-21 21:22:30 -0500

    Added javascript logic for menu nav and div hero image when going from tablet to desktop view (768px to 992px). Finished css for 576px, 768px, and 992px.

--- js/menu.js
+++ js/menu.js
@@ -2,16 +2,67 @@ document.addEventListener('DOMContentLoaded', () => {
   const iconHamburger = document.getElementById('img-hamburger');
   const iconClose = document.getElementById('img-close');
   const nav = document.querySelector('nav');
+  const productsListItem = document.getElementById('list-item-products');
+  const divProductsNav = document.getElementById('div-products-nav');
+  const menuHeader = document.querySelector('.menu-header');
+  const listNav = document.querySelector('.list-nav');
+  const listCTA = document.getElementById('list-cta');
+  const divLandingImg = document.querySelector('.div-landing-img')
 
   iconHamburger.addEventListener('click', (e) => {
     e.preventDefault();
 
-    nav.classList.remove('hide');
+    divProductsNav.classList.remove('hide');
   });
 
   iconClose.addEventListener('click', (e) => {
     e.preventDefault();
 
-    nav.classList.add('hide');
+    divProductsNav.classList.add('hide');
+  });
+
+  productsListItem.addEventListener('click', () => {
+    if (divProductsNav.classList.contains('hide')) {
+      divProductsNav.classList.remove('hide');
+    } else {
+      divProductsNav.classList.add('hide');
+    }
+  });
+
+  if (window.innerWidth < 992) {
+    divProductsNav.remove();
+    nav.appendChild(divProductsNav);
+    listNav.classList.add('hide');
+    menuHeader.classList.remove('hide');
+    iconHamburger.classList.remove('hide');
+    listCTA.classList.add('hide');
+    divLandingImg.classList.add('hide');
+  } else {
+    divProductsNav.remove();  
+    productsListItem.appendChild(divProductsNav);
+    listNav.classList.remove('hide');
+    menuHeader.classList.add('hide');
+    iconHamburger.classList.add('hide');
+    listCTA.classList.remove('hide');
+    divLandingImg.classList.remove('hide');
+  }
+
+  window.addEventListener('resize', () => {
+    divProductsNav.remove();
+    if (window.innerWidth < 992) {
+      nav.appendChild(divProductsNav);
+      listNav.classList.add('hide');
+      menuHeader.classList.remove('hide');
+      iconHamburger.classList.remove('hide');
+      listCTA.classList.add('hide');
+      divLandingImg.classList.add('hide');
+    } else {
+      productsListItem.appendChild(divProductsNav);
+      listNav.classList.remove('hide');
+      menuHeader.classList.add('hide');
+      iconHamburger.classList.add('hide');
+      listCTA.classList.remove('hide');
+      divLandingImg.classList.remove('hide');
+    }
   });
 });
